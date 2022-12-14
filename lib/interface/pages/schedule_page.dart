180cb9244c52b537fce3d8100dff90304f1e4d37
KalitovStudy <nikita.kalitov.study@gmail.com>
2022-12-14 10:51:30 +0400

    third commit

---
+++ lib/interface/pages/schedule_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SchedulePage extends StatefulWidget {
+  const SchedulePage({super.key});
+
+  @override
+  State<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 2, vsync: this);
+  }
+
+  List<Tab> listOfTabs = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: _controller.length, child: Scaffold());
+  }
+}
